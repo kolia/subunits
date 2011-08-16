e0b04d35d83deb85748e77fde2cd694f404b256f
kolia <kolia.sadeghi@gmail.com>
2011-08-16 14:22:52 -0400

    more data for group_LASSO

--- main_group_LASSO.py
+++ main_group_LASSO.py
@@ -30,18 +30,18 @@ filters = np.concatenate(
 # Generate stimulus , spikes , and (STA,STC,mean,cov) of quantities of interest
 R = simulate_retina.LNLNP( nonlinearity=NL, N_cells=N_cells , sigma_spatial=[2.,1.],
                            average_me={'features':lambda x: NL(np.dot(filters,x))},
-                           N_timebins = 100000 )
+                           N_timebins = 500000 )
 
 dSTA = np.concatenate(
             [STA[:,np.newaxis] - R['statistics']['features']['mean'][:,np.newaxis]
-            for STA in R['statistics']['features']['ySTA']], axis=1)
+            for STA in R['statistics']['features']['STA']], axis=1)
 D,Z = schur(R['statistics']['features']['cov'])
 DD  = np.diag(D)
 keep= DD>1e-6
 P   =  (Z[:,keep] * np.sqrt(DD[keep])).T
 y   =  np.dot ( (Z[:,keep] * 1/np.sqrt(DD[keep])).T , dSTA )
 
-V, iW = IRLS( y, P, x=0, disp_every=1000, lam=0.01, maxiter=10000000 , ftol=1e-9, nonzero=1e-3)
+V, iW = IRLS( y, P, x=0, disp_every=1000, lam=0.02, maxiter=10000000 , ftol=0.5e-9, nonzero=1e-3)
 
 def plot_filters(X,same_scale=True):
     for i in range(X.shape[0]):
