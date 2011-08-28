443cb1620eedae5e36a9b57cf1d5f233d47845e7
kolia <kolia.sadeghi@gmail.com>
2011-08-27 21:24:45 -0400

    debugging sparse_group_lasso.py, still

--- sparse_group_lasso.py
+++ sparse_group_lasso.py
@@ -1,6 +1,6 @@
 from   time  import time
 from   numpy import dot, minimum, maximum, sqrt, sign, zeros, arange, floor
-from   numpy import ones, ones_like
+from   numpy import ones, ones_like, array
 import numpy.random   as R
 
 from IPython.Debugger import Tracer; debug_here = Tracer()
@@ -9,28 +9,40 @@ def newton_lasso(pp,pr,coeffL2,gw,w,c):
     sq = sqrt(coeffL2+c**2)
     Lp = -pr + pp*c + gw*c/sq + w*sign(c)
     Lpp=  pp + gw*(1/sq - c**2/sq**3)
-    print 'Lp ',Lp,'   Lpp ',Lpp,'   c ',c
-    return - Lp/Lpp
+#    print 'Lp ',Lp,'   Lpp ',Lpp,'   c ',c
+    return -Lp/Lpp
 
 def optimize_coeffs(predictor,group_weight,weight,rr,coeff):
     # This is ammenable to storing sum(predictor**2) and 
     #             keeping track of sum(predictor*residual) only
-    coeffL2 = sum(coeff[0]**2)
+    coeffL2 = [sum(coeff[0]**2)]
     PP = sum(predictor*predictor,axis=0)
-    for pp,z,c,w in zip( PP, predictor.T, coeff[0], weight ):
-        coeffL2 -= c**2
+    old_coeff = coeff[0]
+    def objective(old_c,coeffL2,z,w,c): 
+        return 0.5*sum((rr[0]-z*(c-old_c))**2) + group_weight*sqrt(coeffL2+c**2) - group_weight*sqrt(coeffL2+old_c**2) + sum(w*abs(c)) - sum(w*abs(old_c))
+    def best_c(coeffL2,pp,z,c,w):
+        old_c = c
+        coeffL2[0] -= c**2
         pr = dot(z,rr[0]) + pp*c
         if abs(pr) < w:
-            c = 0
+            c = 0.
         else:
+            counter = 0
             while 1:
+#                print 'obj pre: ', objective(old_c,coeffL2[0],z,w,c),
                 dc    = newton_lasso(pp,pr,coeffL2,group_weight,w,c)
                 c    += dc
-                pr   += dc * pp
-                rr[0] += dc * z
-                if abs(dc)<1e-5: break
-        coeff[0] = c
-        coeffL2 += c**2
+                counter += 1
+                if abs(dc)<1e-10 or counter>10: break
+#                print '  obj post: ', objective(old_c,coeffL2[0],z,w,c)
+            c = c.squeeze()
+        coeffL2[0] = coeffL2[0] + c**2
+        rr[0] += (old_c-c)*z
+        return c
+
+    coeff[0] = array([best_c(coeffL2,pp,z,c,w) for pp,z,c,w 
+                      in zip( PP, predictor.T, coeff[0], weight )])
+#    rr[0] += dot( predictor, old_coeff-coeff[0] )
 
 def sparsify_group(predictor, group_weight, weight, r, coeff):
     if coeff[0] is None:
@@ -60,7 +72,7 @@ def group_lasso_step(predictors, group_weights, weights, r, coeffs):
     for predictor , group_weight , weight , coeff  in \
     zip(predictors, group_weights, weights, coeffs ):
         sparsify_group (predictor,group_weight,weight,r,coeff)
-        if coeff is not None:
+        if coeff[0] is not None:
             optimize_coeffs(predictor,group_weight,weight,r,coeff)
 
 def test( n=10 , m=300 , s=25 , r=1 ):
@@ -73,12 +85,16 @@ def test( n=10 , m=300 , s=25 , r=1 ):
     predictors    = [P[:,s*i:minimum(s*(i+1),P.shape[1])] for i in range(floor(m/s))]
     group_weights = [1 for _ in predictors]
     weights       = [ones(p.shape[1]) for p in predictors]
-    r,coeffs = initialize_group_lasso(predictors, y, coeffs = None)
+    r,coeffs  = initialize_group_lasso(predictors, y, coeffs = None)
+    def objective():
+        return 0.5*sum(r[0]**2) + \
+        sum([gw*sqrt(sum(c**2)) for gw,c in zip(group_weights,coeffs[0])]) + \
+        sum([sum(w*abs(c))      for w ,c in zip(      weights,coeffs[0])])
     print r
     for _ in range(100):
-        old_coeffs = coeffs
+        old_coeffs = [c[0] for c in coeffs]
         group_lasso_step(predictors, group_weights, weights, r, coeffs)
-        print 'dc: ', [sum(abs(oc[0]-c[0])) for oc,c in 
-                       filter(lambda c: c[0][0] is not None, zip(old_coeffs,coeffs))]
+        print 'objective: ',objective(),'  dc: ', [sum(abs(oc-c[0])) for oc,c in 
+                       filter(lambda c: c[0] is not None and c[1][0] is not None, zip(old_coeffs,coeffs))]
     finish = time()
     print 'Sparse group LASSO ran for ',finish-start,' seconds for n,m: ',n,m
