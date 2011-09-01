47ea9990ba6967601929249989922ab401381fbd
kolia <kolia.sadeghi@gmail.com>
2011-09-01 15:53:55 -0400

    debugging sgl by calculating full objective 2 ways

--- main_group_LASSO.py
+++ main_group_LASSO.py
@@ -56,89 +56,91 @@ except:
                                N_timebins = 5000000 )
     save()
 
-Nspikes_norm  = np.sum(np.sqrt(R['N_spikes']))/len(R['N_spikes'])
+#Nspikes_norm  = np.sum(np.sqrt(R['N_spikes']))/len(R['N_spikes'])
+#
+#dSTA = np.concatenate(
+#            [np.sqrt(Nspikes/2)/Nspikes_norm * 
+#             (STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis])
+#            for Nspikes,STA in 
+#            zip(R['N_spikes'],R['statistics']['features']['STA'])], axis=1)
+            
+total = float( np.sum([Nspikes for Nspikes in R['N_spikes']]) )
 
-dSTA = np.concatenate(
-            [np.sqrt(Nspikes/2)/Nspikes_norm * 
-             (STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis])
-            for Nspikes,STA in 
-            zip(R['N_spikes'],R['statistics']['features']['STA'])], axis=1)
+Nspikes = R['N_spikes']/total
+
+dSTA  = np.concatenate(
+        [STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis]
+        for STA in R['statistics']['features']['STA']], axis=1)
+
 D,Z = schur(R['statistics']['features']['cov']/2)
 DD  = np.diag(D)
 keep= DD>1e-10
 P   =  (Z[:,keep] * np.sqrt(DD[keep])).T
-y   =  np.dot ( (Z[:,keep] * 1/np.sqrt(DD[keep])).T , dSTA )
+y   =  np.dot ( (Z[:,keep] * 1/np.sqrt(DD[keep])).T , dSTA ) / 2
 
-def objective(v,R,y,P):
-    total = np.sum([Nspikes for Nspikes in R['N_spikes']])
-    Nspikes_norm  = np.sum(np.sqrt(R['N_spikes']))/len(R['N_spikes'])
-    P = block_diag(*[P*np.sqrt(Nspikes/2)/Nspikes_norm for Nspikes in R['N_spikes']])
-    y = y.flatten()
+def objective(v,R,y,predictors):
+    total = float( np.sum([Nspikes for Nspikes in R['N_spikes']]) )
+    Nspikes = R['N_spikes']/total
     dSTA  = np.concatenate(
-            [Nspikes/total*
-            (STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis])
-            for Nspikes,STA in 
-            zip(R['N_spikes'],R['statistics']['features']['STA'])], axis=1)
-    direct = np.sum(v*( dSTA - 0.5*np.dot(R['statistics']['features']['cov'],v)))
-    one    = np.ones(v.size)
-    norm   = direct / np.sum( (y-np.dot(P,one))**2 - y**2 )
-    return [ direct , norm*np.sum( (y-np.dot(P,v.flatten()))**2 - y**2 ) ]
+            [STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis]
+            for STA in R['statistics']['features']['STA']], axis=1)    
+    direct = np.sum(v*Nspikes*( dSTA - 0.5*np.dot(R['statistics']['features']['cov'],v)))
+    r,coeffs  = sgl.initialize_group_lasso(predictors, (np.sqrt(Nspikes)*y).flatten())
+    group_weights = [0. for _ in predictors]
+    weights       = [np.zeros(pp.shape[1]) for pp in predictors]
+    return [ direct , sgl.objective(group_weights,weights,r,coeffs) ]
 
 def sobjective(v,R):
-    v = v[:,0]
-
-    dSTA  = R['statistics']['features']['STA'][0][:,np.newaxis]- \
-            R['statistics']['features']['mean'][:,np.newaxis]
-
-    direct = np.sum(v*( dSTA.squeeze() - 0.5*np.dot(R['statistics']['features']['cov'],v)))
-#    direct2= np.dot(v,dSTA) - 0.5*np.dot(v,np.dot(R['statistics']['features']['cov'],v))
-
-    debug_here()
-
+    total = float( np.sum([Nspikes for Nspikes in R['N_spikes']]) )
+    Nspikes = R['N_spikes']/total
+    dSTA  = np.concatenate(
+            [STA[:,np.newaxis]-R['statistics']['features']['mean'][:,np.newaxis]
+            for STA in R['statistics']['features']['STA']], axis=1)
     D,Z = schur(R['statistics']['features']['cov']/2)
+    DD  = np.diag(D)
+    keep= DD>1e-10
+    P   =  (Z[:,keep] * np.sqrt(DD[keep])).T
+    y   =  np.dot ( (Z[:,keep] * 1/np.sqrt(DD[keep])).T , dSTA ) /2
+    
+    direct = np.sum(v*Nspikes*( dSTA - 0.5*np.dot(R['statistics']['features']['cov'],v)))
 
 #    print np.max( D - np.diag(np.diag(D)) )
-    print 'max( Z D Z.T - C ) ', \
-    np.max(np.abs(np.dot(Z,np.dot(D,Z.T)) - R['statistics']['features']['cov']/2))
+#    print 'max( Z D Z.T - C ) ', \
+#    np.max(np.abs(np.dot(Z,np.dot(D,Z.T)) - R['statistics']['features']['cov']/2))
+#    print '2*dot(P.T,y) - dSTA ', np.max(np.abs( 2*np.dot(P.T,y) - dSTA ))
 
-    D = np.diag(D)
-#    print D
-
-    P   =  (Z * np.sqrt(D)).T
-    y   =  np.dot ( (Z * 1/np.sqrt(D)).T , dSTA.squeeze() ) / 2
-
-    print '2*dot(P.T,y) - dSTA ', np.max(np.abs( 2*np.dot(P.T,y) - dSTA ))
-
-    return [ direct , np.sum( - (y-np.dot(P,v))**2 + y**2 ) ]
-#    return [ direct , direct2, np.sum( - (y-np.dot(P,v))**2 + y**2 ) ]
+    return [ direct , np.sum( - (np.sqrt(Nspikes)*(y-np.dot(P,v)))**2 + 
+                                (np.sqrt(Nspikes)*y)**2 ) ]
 
 
 print sobjective(Rand.randn(12,3),R)
-print 
-print
-#print sobjective(Rand.randn(12,3),R)
-#print sobjective(Rand.randn(12,3),R)
+print sobjective(Rand.randn(12,3),R)
+print sobjective(Rand.randn(12,3),R)
 print sobjective(0*Rand.randn(12,3),R)
-print objective(Rand.randn(12,3),R,y,P)
 
 #V, iW = IRLS( y, P, x=0, disp_every=1000, lam=0.15, maxiter=100000 , 
 #              ftol=1e-10, nonzero=1e-1)
 
-#start = time()
-#predictors    = [ block_diag(*[column*np.sqrt(Nspikes/2)/Nspikes_norm
-#                               for Nspikes in R['N_spikes']]).T
-#                  for column in P.T]
-#group_weights = [0.1 for _ in predictors]
-#weights       = [0.01*np.ones(pp.shape[1]) for pp in predictors]
-#
-#r,coeffs  = sgl.initialize_group_lasso(predictors, y.flatten())
-#print r
-#iterations = sgl.sparse_group_lasso(predictors, group_weights, weights, 
-#                                    r, coeffs, maxiter=10000, disp_every=10)
-#finish = time()
-#print iterations,'Iterations of sparse group LASSO in ',finish-start,' seconds for n: ',n
-#print 'infered x ',coeffs
+start = time()
+predictors    = [ block_diag(*[column*np.sqrt(Nspke) for Nspke in Nspikes]).T
+                  for column in P.T]
+                  
+print
+print
+print objective(Rand.randn(12,3),R,y,predictors)
+                  
+                  
+group_weights = [0.1 for _ in predictors]
+weights       = [0.001*np.ones(pp.shape[1]) for pp in predictors]
 
+r,coeffs  = sgl.initialize_group_lasso(predictors, (np.sqrt(Nspikes)*y).flatten())
+print r
+iterations = sgl.sparse_group_lasso(predictors, group_weights, weights, 
+                                    r, coeffs, maxiter=10000, disp_every=10)
+finish = time()
+print iterations,'Iterations of sparse group LASSO in ',finish-start,' seconds for n: ',n
+print 'infered x '
+print sgl.inflate(coeffs)
 
 def plot_filters(X,same_scale=True):
     for i in range(X.shape[0]):
